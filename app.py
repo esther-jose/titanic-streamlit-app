33956803f8ff20f47710e476cd0ca755134594e9
Esther Jose <132091227+esther-jose@users.noreply.github.com>
2025-04-27 19:46:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import joblib
 import numpy as np
 
 # Load model
-model = joblib.load('logistic_model(1).pkl')
+model = joblib.load('logistic_model.pkl')
 
 st.title("🚢 Titanic Survival Prediction App")
 
